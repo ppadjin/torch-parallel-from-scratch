56d65cbc16cd18e61f5f524601b266a9c91900b9
ppadjin <pavle.padjin@gmail.com>
2024-12-14 16:21:17 +0000

    added RingAllReduce class (work in progress)

--- data_parallel.py
+++ data_parallel.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.multiprocessing as mp
@@ -9,14 +10,12 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.nn.parallel import comm
 from abc import ABC, abstractmethod
-from example import get_available_gpus
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"  # Replace with your available GPU indices
 
 
 class DataParallel(ABC):
     def __init__(self, gpu_ids):
         self.gpu_ids = gpu_ids
-        self.num_gpus = len(gpu_ids)
 
     @abstractmethod
     def train(model, gpu_id):
@@ -47,6 +46,20 @@ class DataParallel(ABC):
             if gpu_utilization < th: available_gpus.append(gpu_index)
 
         return available_gpus
+    
+    def backprop_epoch(self, model, dataloader, device):
+        """
+        Perform one backprop epoch and return gradients
+        """
+        model.train()
+        model.zero_grad(set_to_none=True)
+        for _, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device).long()
+            outputs = model(inputs)
+            outputs = F.log_softmax(outputs, dim=-1)
+            loss = self.loss_fn(outputs, targets)
+            loss.backward()
+        return [param.grad.clone() for param in model.parameters()] # collecting gradients
 
 
 class ParameterServer(DataParallel):
@@ -55,8 +68,8 @@ class ParameterServer(DataParallel):
     One GPU is used as a parameter server, while the rest are used as workers.
     """
 
-    def __init__(self, dataloader,num_workers=3, sync_iters=10, bs=32):
-        gpu_ids = get_available_gpus()
+    def __init__(self, dataloader, num_workers=3):
+        gpu_ids = DataParallel.get_available_gpus()
         gpu_ids = gpu_ids[:num_workers + 1]
         self.num_workers = num_workers
         super().__init__(gpu_ids)
@@ -76,8 +89,7 @@ class ParameterServer(DataParallel):
         self.workers_done = mp.Value("i", 0)
 
         processes = []
-        for i, gpu_id in enumerate(self.gpu_ids):
-            #p = mp.Process(target=gpu_worker, args=(gpu_id, model, data_chunk, results_queue))
+        for gpu_id in self.gpu_ids:
             p = mp.Process(target=self.run, args=(gpu_id, model))
             processes.append(p)
             p.start()
@@ -160,7 +172,7 @@ class ParameterServer(DataParallel):
             curr_state_dict = self.model_queue.get()
             local_model.load_state_dict(curr_state_dict)        
 
-            gradients = self.backprop_epoch(local_model, device)
+            gradients = self.backprop_epoch(local_model, self.dataloader, device)
             self.gradients_queue.put((gradients, gpu_id))
 
             print(f"Worker {device}: Completed epoch {epoch + 1}")
@@ -180,18 +192,104 @@ class ParameterServer(DataParallel):
         acc /= len(self.dataloader.dataset)
         return acc
 
-    def backprop_epoch(self, model, device):
-        model.train()
-        model.zero_grad(set_to_none=True)
-        for _, (inputs, targets) in enumerate(self.dataloader):
-            inputs, targets = inputs.to(device), targets.to(device).long()
-            outputs = model(inputs)
-            outputs = F.log_softmax(outputs, dim=-1)
-            loss = self.loss_fn(outputs, targets)
-            loss.backward()
 
-        # All gradients should be collected
-        return [param.grad.clone() for param in model.parameters()]
+class RingAllReduce(DataParallel):
+    def __init__(self, dataloader, num_gpus=3):
+        gpu_ids = DataParallel.get_available_gpus()
+        gpu_ids = gpu_ids[:num_gpus]
+        self.num_workers = num_gpus
+        super().__init__(gpu_ids)
+        
+        self.dataloader = dataloader
+
+    def train(self, model, lr = 0.001, epochs = 1000, loss_fn = nn.CrossEntropyLoss()):
+        self.lr = lr
+        self.epochs = epochs
+        self.loss_fn = loss_fn
+        mp.set_start_method("spawn")
+        self.barrier = mp.Barrier(self.num_workers)
+        processes = self.create_ring_processes(model, self.num_workers)
+
+        for p in processes:
+            p.join()
+
+    def train_ring_node(self, process_id, input_queue, output_queue):
+        """
+        Function to simulate a process in a ring communication pattern.
+        
+        process_id: Unique identifier for this process
+        num_processes: Total number of processes
+        input_queue: Queue to receive data from the previous process
+        output_queue: Queue to send data to the next process
+        """
+
+        # first train for an epoch and get grads
+        gradients = self.backprop_epoch(model, self.dataloader, self.gpu_ids[process_id])
+        
+        # share-reduce
+        for i in range(self.num_workers-1):
+            curr_send_idx = (process_id + i) % self.num_workers
+            rec_idx = (process_id - i - 1) % self.num_workers
+            curr_chunk = np.array_split(gradients, self.num_workers)[curr_send_idx]
+            output_queue.put(curr_chunk)
+            received_chunk = input_queue.get()
+            
+            #gradients += received_chunk
+            self.barrier.wait()
+
+
+        '''for i in range(10):
+            try:
+                # Receive data from the previous process
+                received_data = input_queue.get()
+                print(f"Process {process_id} received: {received_data}")
+                
+                # Modify the data or create a new message
+                new_data = f"{received_data} -> Process {process_id}"
+                
+                # Send to the next process
+                output_queue.put(new_data)
+                print(f"Process {process_id} sent: {new_data}")
+                
+                # Optional: break condition (e.g., after a certain number of iterations)
+                if process_id == 0 and "Process 0 -> full circle" in new_data:
+                    break
+            
+            except:
+                print(f"Process {process_id} timed out waiting for input")
+                break'''
+        
+        print(f"Process {process_id} exiting...")
+
+    def create_ring_processes(self, model, num_processes):
+        """
+        Create N processes in a ring communication pattern.
+        
+        model: Model to train
+        num_processes: Number of processes to create
+        
+        Returns:
+        List of process objects
+        """
+        queues = [mp.Queue() for _ in range(num_processes)]
+        
+        # Create and start processes
+        processes = []
+        for i in range(num_processes):
+            
+            input_queue = queues[(i-1+num_processes)%num_processes] # formula for next node in ring
+            output_queue = queues[i]
+            
+            p = mp.Process(
+                target=self.train_ring_node, 
+                args=(i, input_queue, output_queue))
+            
+            p.start()
+            processes.append(p)
+        
+        return processes
+
+
 
 if __name__ == "__main__":
     N = 100
@@ -200,13 +298,13 @@ if __name__ == "__main__":
     targets = torch.randint(0, n_class, (N,))
     dataset = torch.utils.data.TensorDataset(inputs, targets)
 
-    available_gpus = get_available_gpus()
-    devices = available_gpus[:2]
-
     model = nn.Sequential(nn.Linear(10, 30), nn.ReLU(), nn.Linear(30, n_class))
 
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    param_server = ParameterServer(dataloader, num_workers=2)
-    param_server.train(model, epochs=2000)
+    '''param_server = ParameterServer(dataloader, num_workers=1)
+    param_server.train(model, epochs=2000)'''
 
+
+    ring_all_reduce = RingAllReduce(dataloader, num_gpus=3)
+    ring_all_reduce.train(model, epochs=2000)
     
